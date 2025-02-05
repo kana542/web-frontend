41faeb9b9f24ec2d6ac68dd0a473f778a1f3fb19
chitir <156772506+kana542@users.noreply.github.com>
2025-02-05 14:10:39 +0200

    feat: delete user and user info button

--- fetch/frontend/src/js/fetch.js
+++ fetch/frontend/src/js/fetch.js
@@ -1,46 +1,59 @@
 export const fetchUsers = async () => {
-   try {
-      const response = await fetch("http://localhost:3000/api/users");
-      if (!response.ok) {
-         throw new Error("Failed to fetch users");
-      }
-      console.log("Users fetched");
-      return await response.json();
-   } catch (error) {
-      console.error("Failed to fetch users", error);
-   }
+    try {
+        const response = await fetch("http://localhost:3000/api/users");
+        if (!response.ok) {
+            throw new Error("Failed to fetch users");
+        }
+        console.log("Users fetched");
+        return await response.json();
+    } catch (error) {
+        console.error("Failed to fetch users", error);
+    }
 };
 
 export const createNewUser = async (username, email, password) => {
-   try {
-      const response = await fetch("http://localhost:3000/api/users", {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/json",
-         },
-         body: JSON.stringify({ username, email, password }),
-      });
+    try {
+        const response = await fetch("http://localhost:3000/api/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, email, password }),
+        });
 
-      if (!response.ok) {
-         throw new Error("Failed to create new user");
-      }
-      console.log("New user created");
-   } catch (error) {
-      console.error("Failed to create new user", error);
-   }
+        if (!response.ok) {
+            throw new Error("Failed to create new user");
+        }
+        console.log("New user created");
+    } catch (error) {
+        console.error("Failed to create new user", error);
+    }
 };
 
 export const deleteUser = async (id) => {
-   try {
-      const response = await fetch(`http://localhost:3000/api/users/${id}`, {
-         method: "DELETE",
-      });
+    try {
+        const response = await fetch(`http://localhost:3000/api/users/${id}`, {
+            method: "DELETE",
+        });
 
-      if (!response.ok) {
-         throw new Error("Failed to delete user");
-      }
-      console.log("User deleted");
-   } catch (error) {
-      console.error("Failed to delete user", error);
-   }
+        if (!response.ok) {
+            throw new Error("Failed to delete user");
+        }
+        console.log("User deleted");
+    } catch (error) {
+        console.error("Failed to delete user", error);
+    }
+};
+
+export const getUserById = async (id) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/users/${id}`);
+        if (!response.ok) {
+            throw new Error("Failed to fetch user");
+        }
+        console.log("User fetched");
+        return await response.json();
+    } catch (error) {
+        console.error("Failed to fetch user", error);
+    }
 };
--- fetch/frontend/src/js/main.js
+++ fetch/frontend/src/js/main.js
@@ -1,43 +1,65 @@
-import { fetchUsers, createNewUser, deleteUser } from "./fetch.js";
+import { fetchUsers, createNewUser, deleteUser, getUserById } from "./fetch.js";
 
 const userTableBody = document.getElementById("users-table-body");
 const fetchButton = document.getElementById("fetch-users");
 
 const createUserRow = (user) => {
-   const tr = document.createElement("tr");
+    const tr = document.createElement("tr");
 
-   const usernameTd = document.createElement("td");
-   usernameTd.textContent = user.username;
+    const usernameTd = document.createElement("td");
+    usernameTd.textContent = user.username;
 
-   const emailTd = document.createElement("td");
-   emailTd.textContent = user.email;
+    const emailTd = document.createElement("td");
+    emailTd.textContent = user.email;
 
-   const infoTd = document.createElement("td");
-   const infoButton = document.createElement("button");
-   infoButton.textContent = "Info";
-   infoButton.classList.add("info-button");
-   infoTd.appendChild(infoButton);
+    const infoTd = document.createElement("td");
+    const infoButton = document.createElement("button");
+    infoButton.textContent = "Info";
+    infoButton.classList.add("info-button");
+    infoTd.appendChild(infoButton);
 
-   const deleteTd = document.createElement("td");
-   const deleteButton = document.createElement("button");
-   deleteButton.textContent = "Delete";
-   deleteButton.classList.add("delete-button");
-   deleteTd.appendChild(deleteButton);
+    const deleteTd = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-button");
+    deleteTd.appendChild(deleteButton);
 
-   const idTd = document.createElement("td");
-   idTd.textContent = user.id;
+    const idTd = document.createElement("td");
+    idTd.textContent = user.id;
 
-   tr.append(usernameTd, emailTd, infoTd, deleteTd, idTd);
+    tr.append(usernameTd, emailTd, infoTd, deleteTd, idTd);
 
-   return tr;
+    return tr;
 };
 
 const updateTable = async () => {
-   const users = await fetchUsers();
-   console.log(users);
-   userTableBody.replaceChildren(...users.map((user) => createUserRow(user)));
+    const users = await fetchUsers();
+    userTableBody.replaceChildren(...users.map((user) => createUserRow(user)));
 };
 
-
+const deleteFromTable = async (id) => {
+    await deleteUser(id);
+    updateTable();
+};
 
 fetchButton.addEventListener("click", updateTable);
+userTableBody.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("delete-button")) {
+        const row = event.target.closest("tr");
+        const cells = row.getElementsByTagName("td");
+        const id = cells[4].textContent;
+        await deleteFromTable(id);
+    }
+});
+
+userTableBody.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("info-button")) {
+        const row = event.target.closest("tr");
+        const cells = row.getElementsByTagName("td");
+        const id = cells[4].textContent;
+        const user = await getUserById(id);
+        alert(
+            `Username: ${user.username}\nEmail: ${user.email}\nID: ${user.id}`
+        );
+    }
+});
